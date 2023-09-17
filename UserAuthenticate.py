067ef5556ac04a088b44e13d1475671c586a4871
nich0009 <nich0009@student.monash.edu>
2023-09-17 16:53:46 +0800

    implemented login function and also modify register

--- UserAuthenticate.py
+++ UserAuthenticate.py
@@ -15,57 +15,62 @@ class UserAuthenticate:
         if usertype.lower() in ["younglearner", "educator", "parent"]:
             return True
         
-    def register(self):
+    def register(self, new_firstname, new_lastname, new_password, new_usertype):
         # Auto generate user ID
-        id = max(item['id'] for item in self.data) + 1 if self.data else 1
+            id = max(item['id'] for item in self.data) + 1 if self.data else 1
 
-        # Receiving user firstname
-        new_firstname = input("Enter First Name: ")
-        while not self.validate_name(new_firstname):
-            print("First name cannot be longer than 10 characters and should all be alphatbets. Please try again.")
-            new_firstname = input("Enter First Name: ")
-        new_firstname = new_firstname[0].upper() + new_firstname[1:].lower()
+            # Receiving user firstname
+            # while not self.validate_name(new_firstname):
+            #     print("First name cannot be longer than 10 characters and should all be alphatbets. Please try again.")
+            #     new_firstname = input("Enter First Name: ")
+            new_firstname = new_firstname[0].upper() + new_firstname[1:].lower()
 
-        # Receiving user lastname
-        new_lastname = input("Enter Last Name: ")
-        while not self.validate_name(new_lastname):
-            print("Last name cannot be longer than 10 characters and should all be alphatbets. Please try again.")
-            new_lastname = input("Enter Last Name: ")
-        new_lastname = new_lastname[0].upper() + new_lastname[1:].lower()
+            # Receiving user lastname
+            # new_lastname = input("Enter Last Name: ")
+            # while not self.validate_name(new_lastname):
+            #     print("Last name cannot be longer than 10 characters and should all be alphatbets. Please try again.")
+            #     new_lastname = input("Enter Last Name: ")
+            new_lastname = new_lastname[0].upper() + new_lastname[1:].lower()
 
-        # Create user username based on the name and ID
-        username = new_firstname[:2].lower() + new_lastname[:2].lower() + str(id)
+            # Create user username based on the name and ID
+            username = new_firstname[:2].lower() + new_lastname[:2].lower() + str(id)
 
-        # Create user email based on the name and ID
-        new_email = new_firstname[:2].lower() + new_lastname[:2].lower() + f"{id:04}" + "@codeventure.com"
+            # Create user email based on the name and ID
+            new_email = new_firstname[:2].lower() + new_lastname[:2].lower() + f"{id:04}" + "@codeventure.com"
 
-        #Validate user password
-        new_password = input("Enter Password: ")
-        while not self.validate_password(new_password):
-            print("Password length must be at least 5 and not longer than 15 characters")
-            new_password = input("Enter Password: ")
+            #Validate user password
+            # while not self.validate_password(new_password):
+            #     print("Password length must be at least 5 and not longer than 15 characters")
+            #     new_password = input("Enter Password: ")
 
-        #Validate user usertype
-        new_usertype = input("Enter User Type (either one of these: younglearner, educator, parent): ")
-        while not self.validate_usertype(new_usertype):
-            print("Please enter a valid usertype (younglearner, educator, parent)")
-            new_usertype = input("Enter User Type (either one of these: younglearner, educator, parent):")
-        new_usertype = new_usertype.lower()
+            #Validate user usertype
+            # while not self.validate_usertype(new_usertype):
+            #     print("Please enter a valid usertype (younglearner, educator, parent)")
+            #     new_usertype = input("Enter User Type (either one of these: younglearner, educator, parent):")
+            new_usertype = new_usertype.lower()
 
-        # Create a dictionary for the new object
-        new_object = {
-            "id": id,
-            "firstname": new_firstname,
-            "lastname": new_lastname,
-            "username": username,
-            "email": new_email,
-            "password": new_password,
-            "usertype": new_usertype
-        }
-        # Add the new object to json as a new user
-        self.data.append(new_object)
-        self.db.write_data(self.data)
-        print("New data added successfully")
+            # Create a dictionary for the new object
+            new_object = {
+                "id": id,
+                "firstname": new_firstname,
+                "lastname": new_lastname,
+                "username": username,
+                "email": new_email,
+                "password": new_password,
+                "usertype": new_usertype
+            }
+            # Add the new object to json as a new user
+            self.data.append(new_object)
+            self.db.write_data(self.data)
+            print("New data added successfully\n")
+            print("Your email is: " + new_email)
 
-    def login(self):
+    def login(self, email, password):
+
+        for user in self.data:
+            if user["email"] == email and user["password"] == password:
+                return user["usertype"]
+                
+
+    def reset_password(self):
         pass
--- main.py
+++ main.py
@@ -6,7 +6,8 @@ def main():
     """
     This function controls the overall flow of the CodeVenture game.
     """
-    # Display the Home Page
+
+     # Display the Home Page
     HomePage.display_on_start()
 
     #populate user data from file
@@ -18,16 +19,56 @@ def main():
     auth = UserAuthenticate(db, data)
 
     while True:
-        # display the Home Page Menu
-        HomePage.display_menu()
         menu_input = input("Enter your choice: ")
+        
     #TODO: based on choice, register, login, reset pass or exit
         if menu_input == "1":
-            #TODO: Decide if we need a design for regiser page
-            print("Register")
-            auth.register()
+            #TODO: Decide if we need a design for register page
+            register_first_name = input("Enter First Name: ")
+            if register_first_name == "q":
+                continue
+            
+            register_last_name = input("Enter Last Name: ")
+            if register_last_name == "q":
+                continue
+
+            register_password = input("Enter Password (length must be at least 5 and not longer than 15 characters): ")
+            if register_password == "q":
+                continue
+
+            register_usertype = input("Enter User Type (either one of these: younglearner, educator, parent): ")
+            if register_usertype == "q":
+                continue
+
+            auth.register(register_first_name, register_last_name, register_password, register_usertype)
+
         elif menu_input == "2":
             LoginPage.display_on_start()
+            select_option = input("Select one of these: ")
+            if(select_option == "1"):
+
+                email = input("Please enter your email address: ")
+                if email == "q":
+                    continue
+
+                password =  input("Please enter your password: ")
+                if password == "q":
+                    continue
+
+                role = auth.login(email, password)
+                
+                if(role == "younglearner"):
+                    StudentDashboard.display_dashboard()
+                
+                elif(role == "educator"):
+                    TeacherDashboard.display_dashboard()
+                
+                elif(role == "parent"):
+                    ParentDashboard.display_dashboard()
+                    
+                else:
+                    print("Invalid email/password, please enter correct credentials! ")
+            
         else:
             print("Exiting...")
             break

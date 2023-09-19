0cb6f3cdd1998cb5fb107fd82502190dec9649d6
eun117 <140299144+eun117@users.noreply.github.com>
2023-09-19 18:41:44 +0800

    Implemented all of the functions for student dashboard need to brush up

--- UserInterface.py
+++ UserInterface.py
@@ -1,7 +1,9 @@
 class HomePage:
     @staticmethod
     def display_on_start():
+        print("------------------------")
         print("Welcome to the Home Page")
+        print("------------------------")
         HomePage.display_menu()
 
     @staticmethod
@@ -15,7 +17,9 @@ class HomePage:
 class LoginPage:
     @staticmethod
     def display_on_start():
+        print("-------------------------")
         print("Welcome to the Login Page")
+        print("-------------------------")
         LoginPage.display_menu()
 
     @staticmethod
@@ -24,10 +28,13 @@ class LoginPage:
         print("\t1. Login")
         print("\t2. Reset Password")
 
+
 class StudentDashboard:
     @staticmethod
     def display_dashboard():
+        print("--------------------------------")
         print("Welcome to the Student Dashboard")
+        print("--------------------------------")
         StudentDashboard.display_menu()
 
     @staticmethod
@@ -41,7 +48,9 @@ class StudentDashboard:
 class TeacherDashboard:
     @staticmethod
     def display_dashboard():
+        print("--------------------------------")
         print("Welcome to the Teacher Dashboard")
+        print("--------------------------------")
         TeacherDashboard.display_menu()
 
     @staticmethod
@@ -56,7 +65,9 @@ class TeacherDashboard:
 class ParentDashboard:
     @staticmethod
     def display_dashboard():
+        print("--------------------------------")
         print("Welcome to the Parent Dashboard")
+        print("--------------------------------")
         ParentDashboard.display_menu()
 
     @staticmethod
@@ -65,12 +76,16 @@ class ParentDashboard:
         print("\t1. Check Child's Progress")
         print("\t2. Log Out")
 
+
 class ModulesPage:
     @staticmethod
     def display_on_start(module_array):
+        print("---------------------------")
         print("Welcome to All Modules Page")
+        print("---------------------------")
         print("Please select a module to view: ")
         ModulesPage.display_all_modules(module_array)
+
     def display_all_modules(module_array):
         for module in range(len(module_array)):
             print(f"{module+1}. {module_array[module].module_name}")
@@ -79,7 +94,9 @@ class ModulesPage:
 class ModulePage:
     @staticmethod
     def display_on_start():
+        print("---------------------------")
         print("Welcome to the Modules Page")
+        print("---------------------------")
         ModulePage.display_menu()
 
     @staticmethod
@@ -89,10 +106,13 @@ class ModulePage:
         print("\t2. Take Quiz")
         print("\t3. Back to Dashboard")
 
+
 class ProgressPage:
     @staticmethod
     def display_page():
+        print("----------------------------")
         print("Welcome to the Progress Page")
+        print("----------------------------")
         ProgressPage.display_menu()
         # Display student's progress information here
         # You can fetch and display progress data for the student

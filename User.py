6628a38e5d24c857f237ca8570eacca6ccb0fa04
eun117 <140299144+eun117@users.noreply.github.com>
2023-09-19 18:38:36 +0800

    register function implemented

--- User.py
+++ User.py
@@ -6,13 +6,16 @@ class User:
         self.username = username
         self.email = email
         self.password = password
+
     def __str__(self):
         return f"User: {self.id}, {self.firstname}, {self.lastname}, {self.username}, {self.email}, {self.password}"
+
 
 class YoungLearner(User):
     def __init__(self, id, firstname, lastname, username, email, password):
         super().__init__(id, firstname, lastname, username, email, password)
-    
+
+
 class Educator(User):
     def __init__(self, id, firstname, lastname, username, email, password):
         super().__init__(id, firstname, lastname, username, email, password)
@@ -21,4 +24,3 @@ class Educator(User):
 class Parent(User):
     def __init__(self, id, firstname, lastname, username, email, password):
         super().__init__(id, firstname, lastname, username, email, password)
-    

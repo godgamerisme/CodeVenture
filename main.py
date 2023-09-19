8df0b53ecacbc1e074c9e2588e42ba7cb88cafa8
eun117 <140299144+eun117@users.noreply.github.com>
2023-09-19 18:42:25 +0800

    Added some code to update modules in database

--- main.py
+++ main.py
@@ -4,40 +4,41 @@ from UserAuthenticate import UserAuthenticate
 from User import YoungLearner, Educator, Parent
 from ModuleDatabase import ModuleDatabase
 
+
 def main():
     """
     This function controls the overall flow of the CodeVenture game.
     """
-    #populate user data from file
+    # Populate user data from file
     db = UserDatabase("./data/users.json")
     data = db.read_data()
     users = db.to_user_array()
 
-    #populate module data from file
+    # Populate module data from file
     db_module = ModuleDatabase("./data/modules.json")
     data_module = db_module.read_data()
     modules = db_module.to_module_array(data_module)
 
     # Create instance of UserAuthenticate class
-    auth = UserAuthenticate(db, data,users)
+    auth = UserAuthenticate(db, data, users)
 
     while True:
         # Display the Home Page
         HomePage.display_on_start()
         menu_input = input("Enter your choice: ")
         
-    #TODO: based on choice, register, login, reset pass or exit
+    # TODO: based on choice, register, login, reset pass or exit
         if menu_input == "1":
-            #TODO: Decide if we need a design for register page
+            # TODO: Decide if we need a design for register page
             register_first_name = input("Enter First Name: ")
             if register_first_name == "q":
                 continue
-            
+
             register_last_name = input("Enter Last Name: ")
             if register_last_name == "q":
                 continue
 
-            register_password = input("Enter Password (length must be at least 5 and not longer than 15 characters): ")
+            register_password = input("Enter Password (5 to 15 characters): ")
             if register_password == "q":
                 continue
 
@@ -45,7 +46,7 @@ def main():
             if register_email == "q":
                 continue
 
-            register_usertype = input("Enter User Type (either one of these: younglearner, educator, parent): ")
+            register_usertype = input("Enter User Type (younglearner, educator, parent): ")
             if register_usertype == "q":
                 continue
 
@@ -60,7 +61,7 @@ def main():
                 if username == "q":
                     continue
 
-                password =  input("Please enter your password: ")
+                password = input("Please enter your password: ")
                 if password == "q":
                     continue
 
@@ -72,7 +73,7 @@ def main():
                         StudentDashboard.display_dashboard()
                         student_option = input("Select one of these: ")
                         if student_option == "1":
-                            #List all modules
+                            # List all modules
                             ModulesPage.display_on_start(module_array=modules)
                             module_option = int(input("Select one of these: "))
                             if module_option <= len(modules):
@@ -82,9 +83,9 @@ def main():
                                 ModulePage.display_on_start()
                                 module_page_option = input("Select one of these: ")
 
-                                if module_page_option == "1": #view tutorials
+                                if module_page_option == "1":  # View tutorials
                                     for tutorial in range(len(current_module.tutorials)):
-                                        #display all tutorials title
+                                        # Display all tutorials title
                                         print(f"{tutorial+1}. {current_module.tutorials[tutorial].title}")
                                     tutorial_option = int(input("Select one of these: "))
                                     if tutorial_option <= len(current_module.tutorials):
@@ -92,90 +93,99 @@ def main():
                                         print("You have selected " + current_tutorial.title)
                                         print("Loading tutorial...")
                                         print(current_tutorial.content)
-                                        #prompt user to check if they want to complete the tutorial
-                                        complete_tutorial = input("Do you want to complete this tutorial? (y/n)")
-                                        if complete_tutorial == "y":
-                                            #update the progress in module
-                                            #search for the progress in the module
-                                            for progress in current_module.user_progress:
-                                                if progress.user_id == user.id:
-                                                    progress.completed_tutorials.append(current_tutorial.tutorial_id)
-                                                    #update data in file
-                                                    #search in data_module and append the tutorial id to the completed tutorial
-                                                    for user_progress in data_module["users"]:
-                                                        if user_progress["user_id"] == progress.user_id:
-                                                            #search for the module id in the module progress
-                                                            for module_progress in user_progress["module_progress"]:
-                                                                if module_progress["module_id"] == current_module.module_id:
-                                                                    module_progress["completed_tutorials"].append(current_tutorial.tutorial_id)
-                                                                    break
-                                                    continue
-                                        else:
-                                            continue
-                                elif module_page_option == "2": #Take quiz
+                                        # Prompt user to check if they want to complete the tutorial
+                                        while True:
+                                            complete_tutorial = input("Do you want to complete this tutorial? (y/n): ").lower()  # Case-insensitivity
+                                            if complete_tutorial == "y":
+                                                # Update the progress in module
+                                                # Search for the progress in the module
+                                                for progress in current_module.user_progress:
+                                                    if progress.user_id == user.id:
+                                                        progress.completed_tutorials.append(current_tutorial.tutorial_id)
+                                                        # Update data in file
+                                                        # Search in data_module and append the tutorial id to the completed tutorial
+                                                        for user_progress in data_module["users"]:
+                                                            if user_progress["user_id"] == progress.user_id:
+                                                                # Search for the module id in the module progress
+                                                                for module_progress in user_progress["module_progress"]:
+                                                                    if module_progress["module_id"] == current_module.module_id:
+                                                                        module_progress["completed_tutorials"].append(current_tutorial.tutorial_id)
+                                                                        break
+                                                        break  # Exit the loop if the user input 'y'
+                                            elif complete_tutorial == "n":
+                                                break  # Exit the loop if the user input 'n'
+                                            else:
+                                                print("Please enter 'y' or 'n' to indicate your choice.")
+
+                                elif module_page_option == "2":  # Take quiz
                                     print("You have selected " + current_module.quiz.title)
                                     print("Loading quiz...")
-                                    #display all questions
+                                    # Display all questions
                                     for question in range(len(current_module.quiz.questions)):
                                         print(f"Question{question+1}. {current_module.quiz.questions[question].question_text}")
                                         for option in range(len(current_module.quiz.questions[question].options)):
                                             print(f"Option{option+1}. {current_module.quiz.questions[question].options[option].option_text}")
-                                        #prompt user to select an option
+                                        # Prompt user to select an option
                                         selected_option = input("Select one of the answer: ")
                                         if selected_option == current_module.quiz.questions[question].correct_answer:                                         
                                             print("You have selected the correct answer!")
                                         else:
                                             print("You have selected the wrong answer!")
-                                    #prompt user to check if they want to complete the quiz
-                                    complete_quiz = input("Do you want to complete this quiz? (y/n)")
-                                    if complete_quiz == "y":
-                                        #update the progress in module
-                                        #search for the progress in the module
-                                        for progress in current_module.user_progress:
-                                            if progress.user_id == user.id:
-                                                progress.completed_quiz.append(current_module.quiz.quiz_id)
-                                                #update data in file
-                                                #search in data_module and append the quiz id to the completed quiz
-                                                for user_progress in data_module["users"]:
-                                                    if user_progress["user_id"] == progress.user_id:
-                                                        #search for the module id in the module progress
-                                                        for module_progress in user_progress["module_progress"]:
-                                                            if module_progress["module_id"] == current_module.module_id:
-                                                                module_progress["completed_quiz"].append(current_module.quiz.quiz_id)
-                                                                break
-                                                        break
-                                                continue
-                                    else:
-                                        continue
-                                elif module_page_option == "3": #Back to dashboard
+                                    # Prompt user to check if they want to complete the quiz
+                                    while True:
+                                        complete_quiz = input("Do you want to complete this quiz? (y/n): ").lower()
+                                        if complete_quiz == "y":
+                                            # Update the progress in module
+                                            # Search for the progress in the module
+                                            for progress in current_module.user_progress:
+                                                if progress.user_id == user.id:
+                                                    progress.completed_quiz.append(current_module.quiz.quiz_id)
+                                                    # Update data in file
+                                                    # Search in data_module and append the quiz id to the completed quiz
+                                                    for user_progress in data_module["users"]:
+                                                        if user_progress["user_id"] == progress.user_id:
+                                                            # Search for the module id in the module progress
+                                                            for module_progress in user_progress["module_progress"]:
+                                                                if module_progress["module_id"] == current_module.module_id:
+                                                                    module_progress["completed_quiz"].append(current_module.quiz.quiz_id)
+                                                                    break
+                                                            break
+                                            break  # Exit the loop if the user input 'y'
+                                        elif complete_quiz == "n":
+                                            break  # Exit the loop if the user input 'n'
+                                        else:
+                                            print("Please enter 'y' or 'n' to indicate your choice.")
+
+                                elif module_page_option == "3":  # Back to dashboard
                                     continue
                                 else:   
                                     print("Invalid input, please try again")
                                     continue
-                        elif student_option == "2": #check progress
+
+                        elif student_option == "2":  # Check progress
                             ProgressPage.display_page()
-                            #prompt user to select a progress to view
+                            # Prompt user to select a progress to view
                             progress_option = input("Select one of the option: ")
                             if progress_option == "1":
-                                #go through all the modules and display the progress
+                                # Go through all the modules and display the progress
                                 for module in modules:
                                     print(module.show_user_progress(user.id))
                             elif progress_option == "2":
-                                #back to dashboard
+                                # Back to dashboard
                                 continue
-                        elif student_option == "3": #logout
+                        elif student_option == "3":  # Logout
                             is_logout = True         
                             db.write_data(data)
                             db_module.write_data(data_module)
                     
                     elif isinstance(user, Educator):
                         TeacherDashboard.display_dashboard()
-                        #TODO: IMPLEMENT LTR
+                        # TODO: IMPLEMENT LATER
                         break
                     
                     elif isinstance(user, Parent):
                         ParentDashboard.display_dashboard()
-                        #TODO: IMPLEMENT LTR
+                        # TODO: IMPLEMENT LATER
                         break
                         
                     else:
@@ -191,18 +201,18 @@ def main():
                 if password == "q":
                     continue
                 auth.reset_password(email, password)
-                
-            
+
         else:
             print("Exiting...")
-            #TODO: save data to file
+            print("Shut down completed.")
+            # TODO: save data to file
             
             break
 
     # assuming the user has logged in successfully
 
-
     return
+
 
 if __name__ == '__main__':
     main()
